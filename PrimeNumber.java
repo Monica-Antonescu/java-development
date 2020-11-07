dfeb025b580f57fc1a3368aed2c663b512e03312
Monica-Antonescu <73136863+Monica-Antonescu@users.noreply.github.com>
2020-11-07 18:28:55 +0200

    Homework 1

---
+++ PrimeNumber.java
@@ -0,0 +1,30 @@
+package homework;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        //Display all the prime numbers lower than a given number
+
+        int number = 12345;
+        System.out.print("All prime numbers from 1 to " + number + " are: ");
+        for (int i = 1; i<=number; i++){
+            if (isNumberPrime(i)) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
+
+    private static boolean isNumberPrime(int number){
+        int count = 0;
+        for (int i =2; i<= number/2; i++){
+            if (number % i == 0) {
+                count++;
+            }
+        }
+        if (count == 0) {
+            return true;
+        }
+        return false;
+
+    }
+}
